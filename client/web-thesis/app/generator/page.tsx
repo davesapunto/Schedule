fdd0087b97547059e0679a80b70d6dc2adcf7359
John Dave Sapunto <118431829+davesapunto@users.noreply.github.com>
2024-11-23 03:21:41 +0800

    Update page.tsx

--- client/web-thesis/app/generator/page.tsx
+++ client/web-thesis/app/generator/page.tsx
@@ -92,7 +92,7 @@ export default function ScheduleGenerator() {
 
             console.log("Sending data to server:", dataToSend);
 
-            const response = await fetch('http://127.0.0.1:5000/api/user', {
+            const response = await fetch('/api/user', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
