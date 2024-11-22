01cab19c7b7f033789717296aa6a0f7fa0476cc7
John Dave Sapunto <118431829+davesapunto@users.noreply.github.com>
2024-11-23 00:32:11 +0800

    Update next.config.mjs

--- client/web-thesis/next.config.mjs
+++ client/web-thesis/next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    trailingSlash: true,
     images: {
       unoptimized: true
     },
-    trailingSlash: true
+    eslint: {
+      ignoreDuringBuilds: true, // To suppress ESLint warnings during build
+    }
   }
   
   export default nextConfig
